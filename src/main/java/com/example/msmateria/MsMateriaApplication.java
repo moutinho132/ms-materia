bb7d56e3b762550df17f37bc7ecea4130c18b97e
Fernando Moutinho <moutinho132@gmail.com>
2024-01-17 14:21:45 -0400

    ms-materia

---
+++ src/main/java/com/example/msmateria/MsMateriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.msmateria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMateriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMateriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/msmateria/MsMateriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msmateria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMateriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
